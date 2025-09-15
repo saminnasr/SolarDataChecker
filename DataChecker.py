58d6b989263553e6dd7d008521b51aaecaa87480
Samin <72705951+saminnasr@users.noreply.github.com>
2025-09-15 03:41:15 -0700

    Update DataChecker.py

--- DataChecker.py
+++ DataChecker.py
@@ -55,7 +55,7 @@ for col in formatted_df.columns[1:]:
 html_table = formatted_df.to_html(escape=False, index=False)
 scrollable_table = f"""
 <div style="display: flex; justify-content: center; margin-top:10px;">
-  <div style="width: 100%; max-width: 800px; height: 600px; overflow: auto; border: 1px solid #ddd; padding: 5px;">
+  <div style="width: 100%; max-width: 1200px; height: 600px; overflow: auto; border: 1px solid #ddd; padding: 5px;">
     <style>
       table {{
         border-collapse: collapse;
@@ -67,7 +67,7 @@ scrollable_table = f"""
         vertical-align: middle !important;
         background-color: #f2f2f2;
         color: #000000;
-        padding: 4px 6px;
+        padding: 8px 12px;
       }}
       td {{
         text-align: center;
